03ce0b43153a97cf36e5271e56678bad79edca82
Lucas Stadler <mi.au@papill0n.org>
2011-05-29 15:13:29 +0200

    Give names to DynamicTypes and drop unavaillable interfaces.
    The second is quite stupid, but I didn't know how to do this. Have to
    look a little further, right?

--- lib/gir_ffi/builder/dynamic_type.rb
+++ lib/gir_ffi/builder/dynamic_type.rb
@@ -5,10 +5,13 @@ module GirFFI
 
       def initialize gtype
         @gtype = gtype
+        @type_name = Lib.g_type_name @gtype
       end
 
       def build_class
         CACHE[@gtype] ||= Class.new(parent).tap do |klass|
+          klass_name = @type_name.partition(parent.gir_info.namespace).last
+          parent.const_set klass_name, klass
           klass.const_set :GIR_FFI_BUILDER, parent.gir_ffi_builder
           interfaces.each do |iface|
             klass.class_eval do
@@ -30,8 +33,9 @@ module GirFFI
         iface_types = ::GObject.type_interfaces @gtype
         iface_types.map do |gtype|
           info = gir.find_by_gtype gtype
+          next if info.nil? # missing TypeLib (not loaded)
           GirFFI::Builder.build_class info
-        end
+        end.compact
       end
 
       def gir
